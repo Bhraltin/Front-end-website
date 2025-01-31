84294bbc12c870de2f4f9153c1985a83a3d21678
Bhraltin <bhriskts10@gmail.com>
2025-01-31 23:01:48 +0300

    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min.js'
 import { ThemeContextProvider } from './components/context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <ThemeContextProvider>
-    <App />
-    </ThemeContextProvider>
+  <BrowserRouter>
+    <App/>
     </BrowserRouter>
+    </ThemeContextProvider>
 )
